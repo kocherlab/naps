6458ad3d9804697bf5947bfd12ee52196f36ff58
aewebb80 <19213578+aewebb80@users.noreply.github.com>
2022-07-21 15:28:05 -0400

    Added more metadata

--- naps/__init__.py
+++ naps/__init__.py
@@ -3,9 +3,16 @@ __title__     = "naps"
 __version__   = '0.1'
 __summary__   = "NAPS (NAPS is ArUco Plus SLEAP)"
 __url__       = "https://github.com/kocherlab/naps"
+__code__	  = "https://github.com/kocherlab/naps"
+__issue__	  = "https://github.com/kocherlab/naps/issues"
+__docs__	  = "https://github.com/kocherlab/naps"
 __license__   = "MIT"
-__copyright__ = "Copyright 2022, Scott Wolf, Dee Ruttenberg, Daniel Knapp, Andrew Webb, and Sarah Kocher"
+__copyright__ = "2022"
 
 # Author Information
-__author__    = "Scott Wolf, Dee Ruttenberg, Daniel Knapp, Andrew Webb, and Sarah Kocher"
-__email__     = "scott.w.wolf1@gmail.com, 19213578+aewebb80@users.noreply.github.com"
+__author__  = "Scott Wolf, Dee Ruttenberg, Daniel Knapp, Andrew Webb, and Sarah Kocher"
+__email__   = "scott.w.wolf1@gmail.com, 19213578+aewebb80@users.noreply.github.com"
+
+# Maintainer Information
+__maintainer__       = "Scott Wolf, Andrew Webb",
+__maintainer_email__ = "scott.w.wolf1@gmail.com, 19213578+aewebb80@users.noreply.github.com",
