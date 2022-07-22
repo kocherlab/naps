19b7325e31059a6dc3328529b89242f5816cea57
aewebb80 <19213578+aewebb80@users.noreply.github.com>
2022-07-22 14:15:07 -0400

    Updated hint for adaptiveThreshConstant as floats are supported

--- naps/aruco.py
+++ naps/aruco.py
@@ -3,11 +3,11 @@ from cv2.aruco import Dictionary_get, DetectorParameters_create, detectMarkers
 
 class ArUcoModel ():
 	def __init__ (self,
-				  tag_set,
+				  tag_set:str,
 				  adaptiveThreshWinSizeMin:int = 3,
 				  adaptiveThreshWinSizeMax:int = 30,
 				  adaptiveThreshWinSizeStep:int = 3,
-				  adaptiveThreshConstant:int = 12,
+				  adaptiveThreshConstant:float = 12,
 				  perspectiveRemoveIgnoredMarginPerCell:float = 0.13,
 				  errorCorrectionRate:float = 0.0,
 				  **kwargs):
@@ -55,27 +55,27 @@ class ArUcoModel ():
 	def _assignArucoDict (self, tag_set):
 
 		# Define names of each possible ArUco tag OpenCV supports
-		ARUCO_DICT = {"DICT_4X4_50": cv2.aruco.DICT_4X4_50,
-					  "DICT_4X4_100": cv2.aruco.DICT_4X4_100,
-					  "DICT_4X4_250": cv2.aruco.DICT_4X4_250,
-					  "DICT_4X4_1000": cv2.aruco.DICT_4X4_1000,
-					  "DICT_5X5_50": cv2.aruco.DICT_5X5_50,
-					  "DICT_5X5_100": cv2.aruco.DICT_5X5_100,
-					  "DICT_5X5_250": cv2.aruco.DICT_5X5_250,
-					  "DICT_5X5_1000": cv2.aruco.DICT_5X5_1000,
-					  "DICT_6X6_50": cv2.aruco.DICT_6X6_50,
-					  "DICT_6X6_100": cv2.aruco.DICT_6X6_100,
-					  "DICT_6X6_250": cv2.aruco.DICT_6X6_250,
-					  "DICT_6X6_1000": cv2.aruco.DICT_6X6_1000,
-					  "DICT_7X7_50": cv2.aruco.DICT_7X7_50,
-					  "DICT_7X7_100": cv2.aruco.DICT_7X7_100,
-					  "DICT_7X7_250": cv2.aruco.DICT_7X7_250,
-					  "DICT_7X7_1000": cv2.aruco.DICT_7X7_1000,
-					  "DICT_ARUCO_ORIGINAL": cv2.aruco.DICT_ARUCO_ORIGINAL,
-					  "DICT_APRILTAG_16h5": cv2.aruco.DICT_APRILTAG_16h5,
-					  "DICT_APRILTAG_25h9": cv2.aruco.DICT_APRILTAG_25h9,
-					  "DICT_APRILTAG_36h10": cv2.aruco.DICT_APRILTAG_36h10,
-					  "DICT_APRILTAG_36h11": cv2.aruco.DICT_APRILTAG_36h11}
+		ARUCO_DICT = {'DICT_4X4_50': cv2.aruco.DICT_4X4_50,
+					  'DICT_4X4_100': cv2.aruco.DICT_4X4_100,
+					  'DICT_4X4_250': cv2.aruco.DICT_4X4_250,
+					  'DICT_4X4_1000': cv2.aruco.DICT_4X4_1000,
+					  'DICT_5X5_50': cv2.aruco.DICT_5X5_50,
+					  'DICT_5X5_100': cv2.aruco.DICT_5X5_100,
+					  'DICT_5X5_250': cv2.aruco.DICT_5X5_250,
+					  'DICT_5X5_1000': cv2.aruco.DICT_5X5_1000,
+					  'DICT_6X6_50': cv2.aruco.DICT_6X6_50,
+					  'DICT_6X6_100': cv2.aruco.DICT_6X6_100,
+					  'DICT_6X6_250': cv2.aruco.DICT_6X6_250,
+					  'DICT_6X6_1000': cv2.aruco.DICT_6X6_1000,
+					  'DICT_7X7_50': cv2.aruco.DICT_7X7_50,
+					  'DICT_7X7_100': cv2.aruco.DICT_7X7_100,
+					  'DICT_7X7_250': cv2.aruco.DICT_7X7_250,
+					  'DICT_7X7_1000': cv2.aruco.DICT_7X7_1000,
+					  'DICT_ARUCO_ORIGINAL': cv2.aruco.DICT_ARUCO_ORIGINAL,
+					  'DICT_APRILTAG_16h5': cv2.aruco.DICT_APRILTAG_16h5,
+					  'DICT_APRILTAG_25h9': cv2.aruco.DICT_APRILTAG_25h9,
+					  'DICT_APRILTAG_36h10': cv2.aruco.DICT_APRILTAG_36h10,
+					  'DICT_APRILTAG_36h11': cv2.aruco.DICT_APRILTAG_36h11}
 
 		# Check there are no problem with the tag set
 		if not tag_set: raise Exception('No tag set defined')
