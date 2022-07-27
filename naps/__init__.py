e1214ac7f49f24299dfc9e133e2552145f1614a7
aewebb80 <19213578+aewebb80@users.noreply.github.com>
2022-07-27 15:46:28 -0400

    Minor cleanup for package

--- naps/__init__.py
+++ naps/__init__.py
@@ -1,23 +1,17 @@
 # Basic Information
 __name__ = "naps-track"
-__version__ = "0.1"
+__version__ = "0.1.2"
 __summary__ = "NAPS (NAPS is ArUco Plus SLEAP)"
 __url__ = "https://github.com/kocherlab/naps"
 __code__ = "https://github.com/kocherlab/naps"
 __issue__ = "https://github.com/kocherlab/naps/issues"
-__docs__ = "https://github.com/kocherlab/naps"
+__docs__ = "https://naps.readthedocs.io/en/latest/"
 __license__ = "BSD 3-Clause License"
 __copyright__ = "2022"
 
 # Author Information
-__author__ = "Scott Wolf, Dee Ruttenberg, Daniel Knapp, Andrew Webb, and Sarah Kocher"
-__email__ = "scott.w.wolf1@gmail.com, 19213578+aewebb80@users.noreply.github.com"
-
-# Maintainer Information
-#__maintainer__ = ("Scott Wolf, Andrew Webb",)
-#__maintainer_email__ = (
-#    "scott.w.wolf1@gmail.com, 19213578+aewebb80@users.noreply.github.com",
-#)
+__authors__ = "Scott Wolf, Dee Ruttenberg, Daniel Knapp, Andrew Webb, and Sarah Kocher"
+__email__ = "19213578+aewebb80@users.noreply.github.com"
 
 # Start
 import sys
