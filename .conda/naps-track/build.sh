9c6068323685be848bc3219dac73b425d04f4ea5
aewebb80 <19213578+aewebb80@users.noreply.github.com>
2022-08-03 01:47:16 -0400

    fixed build errors

--- .conda/naps-track/build.sh
+++ .conda/naps-track/build.sh
@@ -4,6 +4,7 @@ export PIP_NO_INDEX=False
 export PIP_NO_DEPENDENCIES=False
 export PIP_IGNORE_INSTALLED=False
 
-pip install opencv-contrib-python-headless
-pip install ray
-python -m pip install .
+python -m pip install opencv-python-headless
+python -m pip install opencv-contrib-python-headless
+python -m pip install ray
+python setup.py install --single-version-externally-managed --record=record.txt
