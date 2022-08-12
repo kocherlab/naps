238132be60a0284c2f412c3970ea7a13ef178f29
aewebb80 <19213578+aewebb80@users.noreply.github.com>
2022-08-12 15:01:11 -0400

    update cost matrix tests

--- tests/test_cost_matrix.py
+++ tests/test_cost_matrix.py
@@ -1,28 +1,29 @@
-import pandas as pd
 import pytest
+
+import numpy as np
+import pandas as pd
 
 from naps.cost_matrix import CostMatrix
 
 
 def test_CostMatrix():
 
-    # Create an example of the CostMatrix input dict
-    value_dict = {
-        0: {"a": [1, 2], "b": [2], "c": [1, 3]},
-        1: {"a": [3], "b": [1, 2], "c": [1, 3]},
-        2: {"a": [3], "b": [1, 2, 3], "c": [3]},
-        3: {"a": [2], "b": [1, 2, 3], "c": []},
-        4: {"a": [2], "b": [1, 2, 3], "c": []},
-    }
+    # Create an example of the CostMatrix input array
+    value_array = np.array([[ 2.,  2.,  3.],
+                            [ 3.,  1.,  3.],
+                            [ 3.,  1.,  3.],
+                            [ 2.,  1., np.nan],
+                            [ 2.,  1., np.nan]])
+
+
 
     # Create the cost matrix, the assign the track/tag pairs
-    cost_matrix = CostMatrix.fromDict(value_dict, 0, 4, 2)
-    assignment_dict = cost_matrix.assignTrackTagPairs()
+    cost_matrix = CostMatrix.fromArray(value_array, 0, 4, 2)
+    assignment_array = cost_matrix.assignTrackTagPairs()
 
     # Confirm the contents of the assignment dictionary
-    assert 2 in assignment_dict
-    assert list(assignment_dict) == [2]
-    assert list(assignment_dict[2]) == ["b", "a", "c"]
+    assert assignment_array.shape == (1, 3)
+    assert list(assignment_array[0]) == [2, 1, 3]
 
 
 def test_CostMatrix_linearAssignment():
