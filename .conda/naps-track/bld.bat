d30b1bcb24a1b0c3c3f3cc4d54a0aa2fd55f824a
Andrew Webb <19213578+aewebb80@users.noreply.github.com>
2023-06-07 12:23:45 -0400

    Update bld.bat

--- .conda/naps-track/bld.bat
+++ .conda/naps-track/bld.bat
@@ -5,7 +5,7 @@ set PIP_NO_DEPENDENCIES=False
 set PIP_IGNORE_INSTALLED=False
 
 pip install opencv-python-headless==4.6.0.66
-pip install opencv-contrib-python-headless=4.6.0.66
+pip install opencv-contrib-python-headless==4.6.0.66
 pip install numpy==1.19.5
 pip install attrs==21.2.0
 pip install wheel==0.35
